0a4405c97614329e63596b25709647321a90ab7e
Gmander <aidanfire360@gmail.com>
2022-04-08 15:20:05 -0400

    notification api implemented. Preparing for setting up frontend for create and get one notification

--- src/features/profile/ProfilePage.tsx
+++ src/features/profile/ProfilePage.tsx
@@ -7,15 +7,14 @@ export default function ProfilePage(props: any) {
 
   console.log("Running getnotofication");
   
-  const resp = getNotification("d5ERictvfpdyQ791YQ3dmnG2jQP2").then(resp => console.log(resp)).catch(err => console.log(err));
-  console.log(resp);
+  getNotification("d5ERictvfpdyQ791YQ3dmnG2jQP2").then(resp => console.log(resp)).catch(err => console.log(err));
+ 
   
   return(
     <>
       <SearchBar />
       {console.log(props) }
       {console.log(useParams)}
-      {console.log(resp)}
       <ProfileInformation beep={props.beep}/>
       
     </>
